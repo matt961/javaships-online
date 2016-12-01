d9209cb9744a9903eafb4b7bac270fa92f4bd9a1
Matt Cocar <matt.cocar@gmail.com>
2016-11-30 20:59:18 -0800

    adds chatting, starting and quitting a game (networking), creates some server test classes

--- src/protocol/JavashipsProtocol.java
+++ src/protocol/JavashipsProtocol.java
@@ -1,22 +1,58 @@
 package protocol;
 
+import java.io.IOException;
 import java.io.PrintWriter;
 
 /**
  * Created by matt on 24/11/16.
+ *
+ * {@link JavashipsProtocol} is used for the creation of commands for {@link client.JavashipsClient}
+ * so that they are distinguishable by the logic.
  */
 public final class JavashipsProtocol {
-        public static final String QUIT = "quit";
-        public static final String ATTACK = "attack";
-        public static final String SEEKING = "seek";
-        public static final String FIRST = "first";
-        public static final String MESSAGE = "msg";
+    public static final String QUIT = "quit";
+    public static final String ATTACK = "attack";
+    public static final String SEEKING = "seek";
+    public static final String FIRST = "first";
+    public static final String MESSAGE = "msg";
 
-        public static void sendMessage(PrintWriter sender, final String message) {
-            sender.println(MESSAGE + "." + message);
+    public static final String SEPARATOR = "-";
+
+    /**
+     * @param sender      The client's PrintWriter, which is connected to {@link server.JavashipsServer}.
+     * @param chatMessage The chat message that the client is sending to the other player.
+     * @throws BadMessageException If the message is blank, don't send it.
+     * @throws IOException         If the PrintWriter is null, stop!
+     *                             <p>
+     *                             Used by {@link client.JavashipsClient} to send chat messages.
+     */
+    public static void sendMessage(PrintWriter sender, final String chatMessage) throws IOException, BadMessageException {
+        if (sender == null) {
+            throw new IOException("The specified PrintWriter is null.");
+        }
+        if (chatMessage.isEmpty()) {
+            throw new BadMessageException("The specified message String is null.");
         }
 
-        public static void sendSeek(PrintWriter sender, int x, int y) {
-            sender.println(SEEKING + "." + x + "." + y);
-        }
+        sender.println(MESSAGE + SEPARATOR + chatMessage);
+    }
+
+    /**
+     * @param sender The client's PrintWriter, which is connected to {@link server.JavashipsServer}.
+     * @param x      The horizontal coordinate of the OppGrid in {@link client.JavashipsClient}
+     *               that the player wishes to attack.
+     * @param y      The vertical coordinate of the OppGrid in {@link client.JavashipsClient}
+     *               that the player wishes to attack.
+     */
+    public static void sendSeek(PrintWriter sender, final int x, final int y) {
+        sender.println(SEEKING + "." + x + "." + y);
+    }
+
+    public static void sendQuit(PrintWriter sender) {
+        sender.println(QUIT);
+    }
+}
+
+class BadMessageException extends Exception {
+    public BadMessageException(String message) { super(message); }
 }
--- src/server/JavashipsServer.java
+++ src/server/JavashipsServer.java
@@ -1,8 +1,5 @@
 package server;
 
-import protocol.JavashipsProtocol;
-import protocol.JavashipsProtocol.*;
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -12,130 +9,139 @@ import java.net.Socket;
 import java.util.Date;
 
 public class JavashipsServer {
-	private static final int PORT = 9876;
+    private static final int PORT = 9876;
 
-	/**
-	 * The main method creates a new JavashipsServer
-	 * that facilitates the sendMessage passing between
-	 * clients. All game logic is processed client-side.
-	 */
-	private JavashipsServer() {
-		try {
-			PrintWriter serverWriter;
+    /**
+     * The main method creates a new JavashipsServer
+     * that facilitates the sendMessage passing between
+     * clients. All game logic is processed client-side.
+     */
+    public JavashipsServer() {
+        try {
+            PrintWriter serverWriter;
 
-			ServerSocket server = new ServerSocket(PORT);
+            ServerSocket server = new ServerSocket(PORT);
 
-			System.out.println("Waiting for 2 players to connect.");
+            System.out.println("Waiting for 2 players to connect.");
 
-			Player player1 = new Player(server.accept());
-			System.out.println(new Date().toString() +
-					" - First player has been found: " +
-					player1.playerSocket.getInetAddress().getHostAddress() +
-					"/" +
-					player1.playerSocket.getPort());
+            Player player1 = new Player(server.accept(), "Player 1");
+            System.out.println(new Date().toString() +
+                    " - First player has been found: " +
+                    player1.playerSocket.getInetAddress().getHostAddress() +
+                    "/" +
+                    player1.playerSocket.getPort());
 
-			Player player2 = new Player(server.accept());
-			System.out.println(new Date().toString() +
-					" - Second player has been found: " +
-					player2.playerSocket.getInetAddress().getHostAddress() +
-					"/" +
-					player2.playerSocket.getPort());
+            Player player2 = new Player(server.accept(), "Player 2");
+            System.out.println(new Date().toString() +
+                    " - Second player has been found: " +
+                    player2.playerSocket.getInetAddress().getHostAddress() +
+                    "/" +
+                    player2.playerSocket.getPort());
 
-			server.close();
-			// done getting players
+            server.close();
+            // done getting players
 
-			serverWriter = new PrintWriter(player1.playerSocket.getOutputStream());
-			serverWriter.println();
+            serverWriter = new PrintWriter(player1.playerSocket.getOutputStream());
+            serverWriter.println();
 
-			player1.setOpponent(player2);
-			player2.setOpponent(player1);
+            player1.setOpponent(player2);
+            player2.setOpponent(player1);
 
-			player1.start();
-			player2.start();
+            player1.start();
+            player2.start();
 
-		} catch (IOException ioe) {
-			ioe.printStackTrace();
-		}
-	}
+        } catch (IOException ioe) {
+            ioe.printStackTrace();
+        }
+    }
 
-	public static void main(String[] args) {
-		System.out.println(new Date().toString() + " - Started server!");
-		new JavashipsServer();
-	}
+    public static void main(String[] args) {
+        System.out.println(new Date().toString() + " - Started server!");
+        new JavashipsServer();
+    }
 
-	/**
-	 * Created by Matt on 11/21/2016.
-	 */
-	class Player extends Thread {
+    /**
+     * Created by Matt on 11/21/2016.
+     */
+    class Player extends Thread {
 
-		private Socket playerSocket;
+        private Socket playerSocket;
 
-		private Player opponentPlayer;
+        private Player opponentPlayer;
 
-		private BufferedReader commandReader;
+        private BufferedReader commandReader;
 
-		private PrintWriter toOpponentCommandWriter;
+        private PrintWriter toOpponentCommandWriter;
 
-		/**
-		 * @param playerSocket The client's socket used for receiving and
-		 *                     sending commands.
-		 * @throws IOException
-		 */
-		Player(final Socket playerSocket) throws IOException {
-			this.playerSocket = playerSocket;
+        private String name;
 
-			commandReader = new BufferedReader(
-					new InputStreamReader(playerSocket.getInputStream()));
-		}
+        /**
+         * @param playerSocket The client's socket used for receiving and
+         *                     sending commands.
+         * @throws IOException
+         */
+        Player(final Socket playerSocket, final String name) throws IOException {
+            this.playerSocket = playerSocket;
+            this.name = name;
 
-		/**
-		 * @param opponentPlayer This player's opponent. The player echoes
-		 *                       a command received to the opponent for parsing.
-		 * @throws IOException
-		 */
-		void setOpponent(final Player opponentPlayer) throws IOException {
-			this.opponentPlayer = opponentPlayer;
+            commandReader = new BufferedReader(
+                    new InputStreamReader(playerSocket.getInputStream()));
+        }
 
-			this.toOpponentCommandWriter = new PrintWriter(
-					getOpponentSocket().getOutputStream());
-		}
+        /**
+         * @param opponentPlayer This player's opponent. The player echoes
+         *                       a command received to the opponent for parsing.
+         * @throws IOException
+         */
+        void setOpponent(Player opponentPlayer) throws IOException {
+            this.opponentPlayer = opponentPlayer;
 
-		Socket getOpponentSocket() {
-			return opponentPlayer.playerSocket;
-		}
+            this.toOpponentCommandWriter = new PrintWriter(
+                    getOpponentSocket().getOutputStream(), true);
+        }
 
-		/**
-		 * @return The command that this Player's client sends.
-		 * @throws IOException
-		 */
-		String getCommandFromPlayer() throws IOException {
-			return commandReader.readLine();
-		}
+        Socket getOpponentSocket() {
+            return opponentPlayer.playerSocket;
+        }
 
-		/**
-		 * @param command The command that getCommandFromPlayer
-		 *                receives is to be sent to the opponent's
-		 *                client for parsing.
-		 * @throws IOException
-		 */
-		void sendCommandToOpponent(final String command) throws IOException {
-			toOpponentCommandWriter.println(command);
-			toOpponentCommandWriter.flush();
-		}
+        /**
+         * @return The command that this Player's client sends.
+         * @throws IOException
+         */
+        String getCommandFromPlayer() throws IOException {
+            return commandReader.readLine();
+        }
 
-		@Override
-		public void run() {
-			while (true) {
-				try {
-					String command = getCommandFromPlayer();
-					System.out.println(new Date().toString() + " :: " + command);
-					sendCommandToOpponent(command);
+        /**
+         * @param command The command that getCommandFromPlayer
+         *                receives is to be sent to the opponent's
+         *                client for parsing.
+         * @throws IOException
+         */
+        void sendCommandToOpponent(final String command) throws IOException {
+            toOpponentCommandWriter.println(command);
+        }
 
-				} catch (IOException e) {
-					System.err.println(new Date().toString() + " - Connection lost.");
-					return;
-				}
-			}
-		}
-	}
+        @Override
+        public void run() {
+
+            String command;
+
+            while (true) {
+                try {
+                    command = getCommandFromPlayer();
+                    if (command == null) {
+                        System.out.println(new Date().toString() + " - " + name + " connection lost.");
+                        return;
+                    }
+                    if (!command.isEmpty()) {
+                        System.out.println(new Date().toString() + " :: " + command);
+                        sendCommandToOpponent(command);
+                    }
+                } catch (IOException e) {
+                    System.err.println(new Date().toString() + " - " + name + " connection lost.");
+                }
+            }
+        }
+    }
 }
